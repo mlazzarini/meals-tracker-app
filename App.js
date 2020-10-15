4b98dfc7650664908f2b52aae75743148be12d28
Margherita Lazzarini <margherita.lazzarini@gmail.com>
2020-10-15 15:17:30 +0200

    Convert indentation to tabs

--- App.js
+++ App.js
@@ -6,47 +6,47 @@ import MealRow from './components/MealRow';
 import SettingsModal from './components/SettingsModal';
 
 export default function App() {
-  const days = ['Lunedi', 'Martedi', 'Mercoledi', 'Giovedi', 'Venerdi', 'Sabato', 'Domenica'];
-  const [ visibleSettings, setVisibleSettings ] = useState(false);
+	const days = ['Lunedi', 'Martedi', 'Mercoledi', 'Giovedi', 'Venerdi', 'Sabato', 'Domenica'];
+	const [ visibleSettings, setVisibleSettings ] = useState(false);
 
-  const initialMeals = days.reduce((acc, day) => { 
-    return {...acc, [day]: ['', '']}
-  }, {} );
-  const [meals, setMeals] = useState(initialMeals);
+	const initialMeals = days.reduce((acc, day) => { 
+		return {...acc, [day]: ['', '']}
+	}, {} );
+	const [meals, setMeals] = useState(initialMeals);
 
-  const setMealValue = (value, day, mealNr) => {
-    const mealsOfTheDay = meals[day];
-    const newMealsOfTheDay = mealNr === 0 ? [value, mealsOfTheDay[1]] : [mealsOfTheDay[0], value];
-    setMeals({...meals, [day]: newMealsOfTheDay});
-  };
+	const setMealValue = (value, day, mealNr) => {
+		const mealsOfTheDay = meals[day];
+		const newMealsOfTheDay = mealNr === 0 ? [value, mealsOfTheDay[1]] : [mealsOfTheDay[0], value];
+		setMeals({...meals, [day]: newMealsOfTheDay});
+	};
 
-  const clearAll = () => {
-    setMeals(initialMeals);
-  };
+	const clearAll = () => {
+		setMeals(initialMeals);
+	};
 
-  return (
-    <View style={styles.container}>
-      <SettingsModal visible={visibleSettings} handleClose={() => setVisibleSettings(false)}/>
-      <ScrollView>
-      {days.map(day =>
-        <View key={day}>
-          <MealRow label={`${day} pranzo`} value={meals[day][0]} setValue={value => setMealValue(value, day, 0)} />
-          <MealRow label={`${day} cena`} value={meals[day][1]} setValue={value => setMealValue(value, day, 1)} />
-        </View>
-      )}
-      </ScrollView>
-      <Button onPress={() => setVisibleSettings(true)} mode="contained" >Settings</Button>
-      <Button onPress={clearAll}> Clear all </Button>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<SettingsModal visible={visibleSettings} handleClose={() => setVisibleSettings(false)}/>
+			<ScrollView>
+			{days.map(day =>
+				<View key={day}>
+					<MealRow label={`${day} pranzo`} value={meals[day][0]} setValue={value => setMealValue(value, day, 0)} />
+					<MealRow label={`${day} cena`} value={meals[day][1]} setValue={value => setMealValue(value, day, 1)} />
+				</View>
+			)}
+			</ScrollView>
+			<Button onPress={() => setVisibleSettings(true)} mode="contained" >Settings</Button>
+			<Button onPress={clearAll}> Clear all </Button>
+			<StatusBar style="auto" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
--- components/MealRow.js
+++ components/MealRow.js
@@ -2,45 +2,45 @@ import React from 'react';
 import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 
 export default function MealRow(props) {
-  const { label, value, setValue } = props;
+	const { label, value, setValue } = props;
 
-  return (
-    <View style={styles.container}>
-        <View style={styles.mealTextContainer}>
-          <Text style={styles.mealText}>{label}:</Text>
-        </View>
-      <TouchableOpacity onLongPress={() => console.log("Long press!!!")}>
-        <View style={styles.mealInputContainer}>
-          <TextInput
-            style={styles.mealInput}
-            onChangeText={text => setValue(text)}
-            value={value}
-            clearButtonMode={'always'}
-            />
-        </View>
-            </TouchableOpacity>
-      </View>
-  );
+	return (
+		<View style={styles.container}>
+				<View style={styles.mealTextContainer}>
+					<Text style={styles.mealText}>{label}:</Text>
+				</View>
+			<TouchableOpacity onLongPress={() => console.log("Long press!!!")}>
+				<View style={styles.mealInputContainer}>
+					<TextInput
+						style={styles.mealInput}
+						onChangeText={text => setValue(text)}
+						value={value}
+						clearButtonMode={'always'}
+						/>
+				</View>
+						</TouchableOpacity>
+			</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flexDirection: 'row',
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    marginVertical: 5,
-  },
-  mealTextContainer: {
-    //flex: 1,
-  },
-  mealInputContainer: {
-    //flex: 3,
-  },
-  mealInput: {
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
-    width: 180,
-  }
+	container: {
+		flexDirection: 'row',
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'space-between',
+		marginVertical: 5,
+	},
+	mealTextContainer: {
+		//flex: 1,
+	},
+	mealInputContainer: {
+		//flex: 3,
+	},
+	mealInput: {
+		height: 40,
+		borderColor: 'gray',
+		borderWidth: 1,
+		width: 180,
+	}
 });
